72d6d1aa3da7c6f6e859347b0e296ee567ca15b1
Bruce Xie <xiediebruce@hotmail.com>
2019-09-07 18:43:01 +0800

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -11,5 +11,23 @@
 	use GetImgFromClipboard\GetImgFromClipboard;
 	
 	$obj = new GetImgFromClipboard();
-	$savedImgPath = $obj->save('/Users/bruce/Downloads/ScreenShot.png');
+	switch (PHP_OS){
+		case 'Darwin':
+			// For macOS, save screenshot to Desktop
+			# /Users/youusername/Desktop/ScreenShot.jpg
+			# /Users/youusername/Desktop/ScreenShot.jpg
+			$savedImgPath = '/Users/bruce/Desktop/ScreenShot.jpg';
+			break;
+		case 'WINNT':
+			// For Windows, save screenshot to Desktop
+			# C:\Users\youusername\Desktop\ScreenShot.jpg
+			# C:\Users\youusername\Desktop\ScreenShot.png
+			$savedImgPath = 'C:\Users\youusername\Desktop\ScreenShot.png';
+		case 'Linux':
+			// For Linux, save screenshot to Desktop
+			# /home/yourusername/Desktop/ScreenShot.jpg
+			# /home/yourusername/Desktop/ScreenShot.png
+			$savedImgPath = '/home/yourusername/Desktop/ScreenShot.jpg';
+	}
+	$savedImgPath = $obj->save($savedImgPath);
 	echo $savedImgPath . "\n";
